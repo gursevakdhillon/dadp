b5766af16cbdbee18592cb2e80d40c1feac59b40
Gursevak Dhillon <dhillonngursevak@gmail.com>
2026-02-26 16:43:13 -0600

    Create external table for fact_trip from stg_payments

--- project3/dw_scripts/fact_payment.sql
+++ project3/dw_scripts/fact_payment.sql
@@ -1,1 +1,14 @@
-
+CREATE EXTERNAL TABLE dbo.fact_trip
+WITH
+(
+    LOCATION    = 'fact/fact_trip/',  
+    DATA_SOURCE = [divvy-file-system_divvydl_dfs_core_windows_net],
+    FILE_FORMAT = [SynapseParquetFormat]
+)
+AS 
+SELECT 
+payment_id,
+date,
+amount,
+rider_id
+FROM dbo.stg_payments 
