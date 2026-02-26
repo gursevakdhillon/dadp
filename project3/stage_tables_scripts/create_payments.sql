f88c815ffbfc40775702521698fc6a21e2377a37
Gursevak Dhillon <dhillonngursevak@gmail.com>
2026-02-26 11:19:20 -0600

    Create external table for staging payments data

--- project3/stage_tables_scripts/create_payments.sql
+++ project3/stage_tables_scripts/create_payments.sql
@@ -1,1 +1,15 @@
+CREATE EXTERNAL TABLE [dbo].[stg_payments] (
+	[payment_id] bigint,
+	[date] datetime2(0),
+	[amount] float,
+	[rider_id] bigint
+	)
+	WITH (
+	LOCATION = 'public.payment.txt',
+	DATA_SOURCE = [divvy-file-system_divvydl_dfs_core_windows_net],
+	FILE_FORMAT = [SynapseDelimitedTextFormat]
+	)
+GO
 
+SELECT TOP 100 * FROM [dbo].[stg_payments]
+GO
