e2f12aed52b2db0180829c43c7893e2588e1fbef
Gursevak Dhillon <dhillonngursevak@gmail.com>
2026-02-26 16:21:26 -0600

    Create external table dim_station from stg_station
    Create an external table 'dim_station' with data from 'stg_station'.

--- project3/dw_scripts/dim_station.sql
+++ project3/dw_scripts/dim_station.sql
@@ -1,1 +1,14 @@
-
+CREATE EXTERNAL TABLE dbo.dim_station
+WITH
+(
+    LOCATION    = 'dim/dim_station/',   -- choose a NEW folder if rerun
+    DATA_SOURCE = [divvy-file-system_divvydl_dfs_core_windows_net],
+    FILE_FORMAT = [SynapseParquetFormat]
+)
+AS 
+SELECT 
+SUBSTRING(station_id,2,LEN(station_id) -2) AS station_id,
+(SUBSTRING(name,2,LEN(name)-2)) AS name,
+latitude,
+longitude
+FROM dbo.stg_station
