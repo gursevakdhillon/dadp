769efa6cd047ba8d3299bfe0cb7b370acc8f98b4
Gursevak Dhillon <dhillonngursevak@gmail.com>
2026-02-26 11:21:47 -0600

    Create external table for station data

--- project3/stage_tables_scripts/create_stations.sql
+++ project3/stage_tables_scripts/create_stations.sql
@@ -1,1 +1,16 @@
+CREATE EXTERNAL TABLE [dbo].[station] (
+	[station_id] nvarchar(400),
+	[name] nvarchar(400),
+	[latitude] float,
+	[longitude] float
+	)
+	WITH (
+	LOCATION = 'public.station.txt',
+	DATA_SOURCE = [divvy-file-system_divvydl_dfs_core_windows_net],
+	FILE_FORMAT = [SynapseDelimitedTextFormat]
+	)
+GO
 
+
+SELECT TOP 100 * FROM [dbo].[station]
+GO
