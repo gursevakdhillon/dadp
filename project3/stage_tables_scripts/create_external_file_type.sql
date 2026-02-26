4618b4fb3048e7276b6cf94350d56a6e50138a77
Gursevak Dhillon <dhillonngursevak@gmail.com>
2026-02-26 11:18:53 -0600

    Delete project3/stage_tables_scripts/create_external_file_type.sql

--- project3/stage_tables_scripts/create_external_file_type.sql
+++
@@ -1,9 +0,0 @@
-IF NOT EXISTS (SELECT * FROM sys.external_file_formats WHERE name = 'SynapseDelimitedTextFormat') 
-	CREATE EXTERNAL FILE FORMAT [SynapseDelimitedTextFormat] 
-	WITH ( FORMAT_TYPE = DELIMITEDTEXT ,
-	       FORMAT_OPTIONS (
-			 FIELD_TERMINATOR = ',',
-			 FIRST_ROW = 2,
-			 USE_TYPE_DEFAULT = FALSE
-			))
-GO
