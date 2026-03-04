a5ea3c018451d092069f71eef810fe3d79328055
Gursevak Dhillon <dhillonngursevak@gmail.com>
2026-03-04 16:10:30 -0600

    Create fact_trip table and update SQL query

--- project3/dw_scripts/fact_trip.sql
+++ project3/dw_scripts/fact_trip.sql
@@ -1,21 +1,19 @@
-CREATE EXTERNAL TABLE dbo.fact_payment
+CREATE EXTERNAL TABLE dbo.fact_trip
 WITH
 (
-    LOCATION    = 'fact/fact_payment/',  
+    LOCATION    = 'fact/fact_trip/',  
     DATA_SOURCE = [divvy-file-system_divvydl_dfs_core_windows_net],
     FILE_FORMAT = [SynapseParquetFormat]
 )
 AS 
-SELECT
-SUBSTRING(trip_id,2,LEN(trip_id) -2) AS trip_id,
-SUBSTRING(rideable_type,2,LEN(rideable_type)-2) AS rideable_type,
-start_at AS started_at,
-ended_at AS ended_at,
-DATEDIFF(MINUTE, start_at,ended_at) AS trip_duration,
-SUBSTRING(start_station_id,2, LEN(start_station_id)) AS start_station_id,
-SUBSTRING(end_station_id,2,LEN(end_station_id)) AS end_station_id,
-r.rider_id,
-DATEDIFF(YEAR,birthday,start_at) AS rider_age
-FROM dbo.stg_trip t
-LEFT JOIN dbo.stg_rider r
-ON t.rider_id  = r.rider_id
+SELECT 
+payment_id,
+fr.rider_id AS rider_id,
+dd.date_id     AS date_id,
+amount
+FROM dbo.stg_payments sp
+JOIN dim_rider fr 
+ON sp.rider_id = fr.rider_id
+JOIN dim_date dd
+ON sp.date = dd.date
+
